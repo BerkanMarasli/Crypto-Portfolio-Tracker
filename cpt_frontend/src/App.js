9a1f114c54b8c20b35afe2f80e2f006c42478a0d
Berkan Marasli <berkan.marasli@hotmail.com>
2021-10-02 18:38:01 +0100

    Removed 'view bitcoin' - completed 'view all crypto'

--- cpt_frontend/src/App.js
+++ cpt_frontend/src/App.js
@@ -1,9 +1,7 @@
 // https://docs.coincap.io/ => main crypto information
-// https://cryptoicons.org/ => crypto logos
 // ^^ https://github.com/spothq/cryptocurrency-icons => crypto logos
 // ^^ https://cryptologos.cc/
 
-// import BITCOINLOGO from "./cryptoLogos/icon/btc.png"
 import "./App.css"
 import Button from "./components/Button"
 import { useState } from "react"
@@ -11,22 +9,12 @@ import CryptoInfo from "./components/CryptoInfo"
 
 export default function App() {
   const [isClickedViewAllCrypto, setIsClickedViewAllCrypto] = useState(false)
-  const [isClickedViewBitcoin, setIsClickedViewBitcoin] = useState(false)
-  const [bitcoinData, setBitcoinData] = useState({})
   const [allCryptoData, setAllCryptoData] = useState([])
-  // const [bitcoinLogo, setBitcoinLogo] = useState("")
 
   return (
     <div className="App">
       <p>Crypto Portfolio Tracker</p>
       <br />
-      <Button
-        nameOfButton="View Bitcoin"
-        handleClick={handleViewBitcoinClick}
-        setIsClickedState={setIsClickedViewBitcoin}
-        setData={setBitcoinData}
-        // setLogo={setBitcoinLogo}
-      />
       <Button
         nameOfButton="View all CRYPTOCURRENCIES"
         handleClick={handleViewAllCryptoClick}
@@ -34,19 +22,9 @@ export default function App() {
         setData={setAllCryptoData}
       />
       <br />
-      {isClickedViewBitcoin === true ? <CryptoInfo data={bitcoinData} /> : null}
       {isClickedViewAllCrypto === true ? allCryptoData : null}
-      {/* <img src={BITCOINLOGO} alt="IDK"></img> */}
     </div>
   )
-}
-
-async function handleViewBitcoinClick(setIsClickedViewBitcoin, setBitcoinData) {
-  setIsClickedViewBitcoin(true)
-  const URL = "https://api.coincap.io/v2/assets/bitcoin"
-  const data = await getDataFromCoinCapAPI(URL)
-  const bitcoinData = data.data
-  setBitcoinData(bitcoinData)
 }
 
 async function handleViewAllCryptoClick(setIsClickedViewAllCrypto, setAllCryptoData) {
@@ -61,43 +39,25 @@ async function handleViewAllCryptoClick(setIsClickedViewAllCrypto, setAllCryptoD
 }
 
 async function getDataFromCoinCapAPI(url) {
-  try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer 5d7f001c-d807-4046-bcca-dfe169fa1ca1",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS, PUT, PATCH",
-        "Access-Control-Allow-Headers":
-          "Origin, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-      },
-    })
-    return await response.json()
-  } catch (error) {
-    console.log("COULDNT FETCH DATA")
-    console.log(error)
+  let response
+  let dataFound = false
+  while (!dataFound) {
+    try {
+      response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer 5d7f001c-d807-4046-bcca-dfe169fa1ca1",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS, PUT, PATCH",
+          "Access-Control-Allow-Headers":
+            "Origin, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, append, delete, entries, foreach, get, has, keys, set, values, Authorization",
+        },
+      })
+      dataFound = true
+    } catch (error) {
+      console.log(error)
+    }
   }
+  return await response.json()
 }
-
-// async function getCryptoLogo(cryptoTicker) {
-//   const ticker = cryptoTicker.toLowerCase()
-//   const url = `https://cryptoicons.org/api/icon/${ticker}/32`
-//   try {
-//     const response = await fetch(url, {
-//       method: "GET",
-//       headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS, PUT, PATCH",
-//         "Access-Control-Allow-Headers":
-//           "Origin, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-//       },
-//     })
-//     const logo = await response.blob()
-//     const logoUrl = URL.createObjectURL(logo)
-//     return logoUrl
-//   } catch (error) {
-//     console.log("COULDNT FETCH LOGO")
-//     console.log(error)
-//   }
-// }
